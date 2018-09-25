d3c06946dec1563c2dda481eff8206a208390583
sginabreda <santi.ginabreda@gmail.com>
2018-09-25 00:46:33 -0300

    Formularios Peliculas y Funciones

--- src/main/java/com/applicacionesInteractivas/vista/formularios/funciones/FormElimFuncion.java
+++ src/main/java/com/applicacionesInteractivas/vista/formularios/funciones/FormElimFuncion.java
@@ -1,12 +1,84 @@
 package com.applicacionesInteractivas.vista.formularios.funciones;
 
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+import com.applicacionesInteractivas.controllers.CineController;
+import com.applicacionesInteractivas.vista.formularios.tabla.TablaFunciones;
 
 public class FormElimFuncion extends JFrame{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 6560295143013798697L;
+	private JButton btnEliminar;
+	private JTable tablaFunciones;
+	private TablaFunciones tablaFuncionesModel;
+	private JScrollPane miBarra;
+	private JPanel btnContainer, tableContainer;
+	private JPanel mainPanel;
 
+	public FormElimFuncion() {
+		
+		this.setSize(320, 360);
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);
+		this.setTitle("Eliminar Funcion");
+		mainPanel = new JPanel();
+		
+		btnEliminar = new JButton("Eliminar");
+		btnEliminar.addActionListener(e -> {
+			CineController cine = CineController.getInstance();
+			cine.eliminarFuncion((String) tablaFunciones.getValueAt(tablaFunciones.getSelectedRow(), 0));
+			JOptionPane.showMessageDialog(null,"Funcion modificada!");
+			this.setVisible(false);
+		});
+		btnEliminar.setEnabled(false);
+		this.add(btnEliminar);
+		
+		tablaFunciones = new JTable();
+		tablaFunciones.setPreferredScrollableViewportSize(new Dimension(500, 70));
+		tablaFunciones.addMouseListener(new MouseAdapter() {
+		    public void mousePressed(MouseEvent mouseEvent) {
+		        JTable table =(JTable) mouseEvent.getSource();
+		        Point point = mouseEvent.getPoint();
+		        int row = table.rowAtPoint(point);
+		        if (mouseEvent.getClickCount() == 1 && table.getSelectedRow() != -1) {
+		            try{
+			            btnEliminar.setEnabled(true);
+		            }
+		            catch(Exception e){
+		            	e.printStackTrace();
+		            }
+		        }
+		    }
+		});
+		
+		miBarra = new JScrollPane();
+		miBarra.setBounds(40, 300, 400, 130);
+		
+		tablaFuncionesModel = new TablaFunciones();
+		tablaFuncionesModel.setFunciones(CineController.getInstance().getFunciones());
+		
+		tablaFunciones.setModel(tablaFuncionesModel);
+		miBarra.setViewportView(tablaFunciones);
+		
+		btnContainer = new JPanel();
+		btnContainer.add(btnEliminar);
+		
+		tableContainer = new JPanel();
+		tableContainer.add(miBarra);
+		
+		mainPanel.add(btnContainer);
+		mainPanel.add(tableContainer);
+		
+		getContentPane().add(mainPanel);
+	}
 }
--- src/main/java/com/applicacionesInteractivas/vista/formularios/peliculas/FormElimPelicula.java
+++ src/main/java/com/applicacionesInteractivas/vista/formularios/peliculas/FormElimPelicula.java
@@ -1,12 +1,84 @@
 package com.applicacionesInteractivas.vista.formularios.peliculas;
 
+import java.awt.Dimension;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+import com.applicacionesInteractivas.controllers.CineController;
+import com.applicacionesInteractivas.vista.formularios.tabla.TablaPeliculas;
 
 public class FormElimPelicula extends JFrame{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -4223846769846167012L;
-
+	private JButton btnEliminar;
+	private JTable tablaPeliculas;
+	private TablaPeliculas tablaPeliculasModel;
+	private JScrollPane miBarra;
+	private JPanel mainPanel;
+	private JPanel tableContainer;
+	private JPanel btnContainer;
+	
+	public FormElimPelicula() {
+		
+		this.setSize(500, 500);
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);
+		this.setTitle("Eliminar Pelicula");
+		mainPanel = new JPanel();
+		
+		
+		btnEliminar = new JButton("Eliminar");
+		btnEliminar.addActionListener(e -> {
+			CineController cine = CineController.getInstance();
+			cine.eliminarPelicula((String) tablaPeliculas.getValueAt(tablaPeliculas.getSelectedRow(), 0));
+			JOptionPane.showMessageDialog(null,"Pelicula eliminada!");
+			this.setVisible(false);
+		});
+		btnEliminar.setEnabled(false);
+		this.add(btnEliminar);
+		
+		tablaPeliculas = new JTable();
+		tablaPeliculas.setPreferredScrollableViewportSize(new Dimension(500, 70));
+		tablaPeliculas.addMouseListener(new MouseAdapter() {
+		    public void mousePressed(MouseEvent mouseEvent) {
+		        JTable table =(JTable) mouseEvent.getSource();
+		        if (mouseEvent.getClickCount() == 1 && table.getSelectedRow() != -1) {
+		            try{
+			            btnEliminar.setEnabled(true);
+		            }
+		            catch(Exception e){
+		            	e.printStackTrace();
+		            }
+		        }
+		    }
+		});
+		
+		miBarra = new JScrollPane();
+		miBarra.setBounds(40, 300, 400, 130);
+		
+		tablaPeliculasModel = new TablaPeliculas();
+		tablaPeliculasModel.setPeliculas(CineController.getInstance().getPeliculas());
+		
+		tablaPeliculas.setModel(tablaPeliculasModel);
+		tablaPeliculas.setDragEnabled(false);
+		miBarra.setViewportView(tablaPeliculas);
+		
+		btnContainer = new JPanel();
+		btnContainer.add(btnEliminar);
+		
+		tableContainer = new JPanel();
+		tableContainer.add(miBarra);
+		
+		mainPanel.add(btnContainer);
+		mainPanel.add(tableContainer);
+		
+		getContentPane().add(mainPanel);
+	}
 }
