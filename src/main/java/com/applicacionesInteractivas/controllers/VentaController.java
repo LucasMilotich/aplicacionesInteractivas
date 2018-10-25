0c3bb1b1ee2582c1312f2464a0a831d8064fa944
Lucas Milotich <lucas.milotich@rappi.com>
2018-10-25 16:11:15 -0300

    firma

--- src/main/java/com/applicacionesInteractivas/controllers/VentaController.java
+++ src/main/java/com/applicacionesInteractivas/controllers/VentaController.java
@@ -3,7 +3,9 @@ package com.applicacionesInteractivas.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.applicacionesInteractivas.modelo.Venta;
+import com.applicacionesInteractivas.modelo.*;
+import com.applicacionesInteractivas.modelo.descuento.Descuento;
+import com.applicacionesInteractivas.modelo.descuento.DescuentoComposite;
 import com.applicacionesInteractivas.vista.formularios.ventas.VentaBoleteria;
 
 public class VentaController {
@@ -24,8 +26,29 @@ public class VentaController {
 		this.ventaBoleteria = ventaBoleteria;
 	}
 	
-    public void venderBoleteria(String cineCuil, String nombrePelicula, String sala, String horario, int cantidad, String formaPago, List<String> descuentoNombre){
+    public void venderBoleteria(String cineCuil, String nombrePelicula, String salaNombre, String horario, int cantidad, String formaPago, List<String> descuentoNombre){
+        // precio harcodeado
 
+        List<Descuento>  descuentos = DescuentoController.getInstance().getDescuentos();
+        Descuento descuentoAAplicar = buildCompositeDescuento(descuentos);
+
+        Cine cine = CineController.getInstance().getCine(cineCuil);
+        Pelicula pelicula = CineController.getInstance().getPelicula(nombrePelicula);
+        Sala sala = CineController.getInstance().getSala(cineCuil,salaNombre);
+        Funcion funcion = CineController.getInstance().getFuncion(cineCuil,nombrePelicula,salaNombre, horario);
+
+        Venta
+
+
+    }
+
+    private Descuento buildCompositeDescuento(List<Descuento> descuentos){
+        DescuentoComposite d = new DescuentoComposite();
+        for (Descuento desc : descuentos){
+            d.getDescuentos().add(desc);
+        }
+
+        return d;
     }
 
 }
